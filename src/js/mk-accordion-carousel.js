f09de0fa1ecccacdb0751f92c40292d05bc242f5
Masoom <shaik.masoom@edgenetworks.in>
2016-02-23 11:29:37 +0530

    basic working carousel

--- src/js/mk-accordion-carousel.js
+++ src/js/mk-accordion-carousel.js
@@ -31,18 +31,45 @@ mkACApp.service('mkAC', [function () {
   }
 }]);
 
-mkACApp.controller('mkACCtrl', function($scope, mkAC){
+mkACApp.controller('mkACCtrl', function($scope ,$interval ,mkAC){
   // $scope.$watch(function(){
-  //   return mkBlocker.isBlocked;
+  //   console.log(mkAC.itemsList);
+  //   return mkAC.itemsList;
   // }, function(){
-  //   $scope.isBlocked = mkBlocker.isBlocked;
+  //   console.log("changed");
+  //   // $scope.isBlocked = mkBlocker.isBlocked;
   // });
+  $scope.items = [];
+  console.log($scope.items);
+  $scope.checkItems = function(obj, element){
+    // mkAC.itemsList = obj;
+    $scope.items = obj;
+
+    console.log(obj.length);
+    if(obj.length>0){
+      console.log($scope.items);
+      mkAC.orderCarousel(element);
+    }
+  }
   $scope.orderCarousel = function(element){
-    console.log(angular.element(element));
+    console.log($scope.items);
     mkAC.orderCarousel(element);
   }
-  $scope.reorderCarousel = function($event){
+  $scope.reorderCarousel = function($event, item, itemsArray){
+    // mkAC.reorderCarousel($event);
+    // mkAC.itemsList.unshift(item);
+    // index = mkAC.itemsList.indexOf(item);
+    // if(index !== -1){
+    //   mkAC.itemsList.splice(index, 1);
+    //   mkAC.itemsList.unshift(item);
+    // }
+    // index = itemsArray.indexOf(item);
+    // if(index !== -1){
+    //   itemsArray.splice(index, 1);
+    //   itemsArray.unshift(item);
+    // }
     mkAC.reorderCarousel($event);
+    // console.log(mkAC.itemsList);
   }
 });
 
@@ -50,9 +77,11 @@ mkACApp.directive('mkItem', [function () {
    return {
       restrict: 'AE',
       controller: 'mkACCtrl',
-      link: function (scope, element, iAttrs) {
+      link: function (scope, element, attrs) {
         if(scope.$last) {
-          scope.orderCarousel(element);
+          var obj = scope.$eval(attrs.mkItems);
+          console.log(obj);
+          scope.checkItems(obj, element);
         }
       }
    };
